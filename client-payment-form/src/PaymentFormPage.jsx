76ae9fc92e3d18094b1476a22256b575d4dfbcb8
simon-m-dignite <shoaib.muhammad@launchbox.pk>
2024-10-23 05:44:17 +0500

    added pb_stripe

--- client-payment-form/src/PaymentFormPage.jsx
+++ client-payment-form/src/PaymentFormPage.jsx
@@ -2,7 +2,9 @@ import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 import PaymentForm from "./PaymentForm";
 const KEY = import.meta.env.VITE_STRIPE_PUBLIC_KEY;
-const stripePromise = loadStripe(KEY);
+const stripePromise = loadStripe(
+  "pk_live_51PgA9YC8RyYZZPssTu0IgALckWS1Wgm8LbvlstOxTf4okOKi60K341V3xZH1fmQKKJs1NVrL6f6bt8HmwFKVBKnB00jnGo8bZo"
+);
 
 function PaymentFormPage() {
   return (
