2d8b827c4e7b8de5f823db1cbc0a62679f866494
simon-m-dignite <shoaib.muhammad@launchbox.pk>
2024-10-26 05:44:18 +0500

    changed stripe keys

--- client-payment-form/src/PaymentFormPage.jsx
+++ client-payment-form/src/PaymentFormPage.jsx
@@ -4,14 +4,14 @@ import PaymentForm from "./PaymentForm";
 import { useEffect } from "react";
 const KEY = import.meta.env.VITE_STRIPE_PUBLIC_KEY;
 // fit110 pb_key
-// const stripePromise = loadStripe(
-//   "pk_live_51PgA9YC8RyYZZPssTu0IgALckWS1Wgm8LbvlstOxTf4okOKi60K341V3xZH1fmQKKJs1NVrL6f6bt8HmwFKVBKnB00jnGo8bZo"
-// );
+const stripePromise = loadStripe(
+  "pk_live_51PgA9YC8RyYZZPssTu0IgALckWS1Wgm8LbvlstOxTf4okOKi60K341V3xZH1fmQKKJs1NVrL6f6bt8HmwFKVBKnB00jnGo8bZo"
+);
 
 // Dignite pb_key
-const stripePromise = loadStripe(
-  "pk_live_51LFwmKA6pffrHFlzgfCnZns4MnSgd7MYsktlyadTvw5lHBwCUk8VZId2asjmXGD1wGYe4iIJpuZhFqJvNFAxc7qZ00DX3OMpir"
-);
+// const stripePromise = loadStripe(
+//   "pk_live_51LFwmKA6pffrHFlzgfCnZns4MnSgd7MYsktlyadTvw5lHBwCUk8VZId2asjmXGD1wGYe4iIJpuZhFqJvNFAxc7qZ00DX3OMpir"
+// );
 
 function PaymentFormPage() {
   return (
