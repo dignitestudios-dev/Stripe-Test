ea08dc024243435bd5608775981606371fa7fd48
simon-m-dignite <shoaib.muhammad@launchbox.pk>
2024-10-31 05:03:33 +0500

    changed api on admin dashboard

--- admin/src/api/api.js
+++ admin/src/api/api.js
@@ -1,4 +1,4 @@
-export const BASE_URL = `https://authorizedbilling.com/api`;
-export const IMAGE_URL = `https://authorizedbilling.com/`;
+export const BASE_URL = `https://backend.authorizedbilling.com/api`;
+export const IMAGE_URL = `https://backend.authorizedbilling.com/`;
 // export const BASE_URL = `http://localhost:2000/api`;
 // export const IMAGE_URL = `http://localhost:2000/`;
